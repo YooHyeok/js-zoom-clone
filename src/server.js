cd4beb3e15b2fe8f22140cffa161275c3c65e899
webdevyoo <yjk7454@naver.com>
2023-12-17 01:14:55 +0900

    pug파일 추가후 서버파일에서 뷰 템플릿 확장자 /경로 등록 및 렌더링 설정

--- src/server.js
+++ src/server.js
@@ -1,4 +1,13 @@
 import express from "express"
 const app = express();
-console.log("hello")
-app.listen(3000)
+
+app.set('view engine', 'pug') //view 엔진을 pug 로 설정 (spring의 view resolver처럼 suffix에 해당)
+app.set('views', __dirname + "/views") // root(src)/views 하위의 pug파일을 읽는다.
+
+app.get("/", (req, res)=>{
+  res.render("home") // get함수에 지정한 매핑주소로 접속시 home을 렌더링한다. render에 전달받은 매개변수는 view정보이며 파일명의 prefix에 해당한다. (spring의 view resolver처럼 home.pug로 변한됨)
+})
+
+const port = 3000;
+const handleListen = () => console.log(`Listening on http://localhost:${port}`)
+app.listen(port, handleListen)
