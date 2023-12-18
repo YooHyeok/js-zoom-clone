ffc02e84de4203bb28686b1b12ae45a90708fbba
webdevyoo <yjk7454@naver.com>
2023-12-18 09:23:58 +0900

    모든 요청 '/' url path 로 redirect - 어떠한 요청이 와도 home을 렌더링

--- src/server.js
+++ src/server.js
@@ -11,7 +11,9 @@ app.use("/public", express.static(__dirname + "/public")) // 정적파일 공유
 app.get("/", (req, res)=>{
   res.render("home") // get함수에 지정한 매핑주소로 접속시 home을 렌더링한다. render에 전달받은 매개변수는 view정보이며 파일명의 prefix에 해당한다. (spring의 view resolver처럼 home.pug로 변한됨)
 })
+app.get("/*", (req, res) => res.redirect("/")) // 어떠한 요청이 와도 home을 렌더링한다.
 
 const port = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${port}`)
+
 app.listen(port, handleListen)
