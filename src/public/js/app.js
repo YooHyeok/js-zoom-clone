637da66ee37521e72a91f368d491914bd1478b02
webdevyoo <yjk7454@naver.com>
2023-12-19 01:18:49 +0900

    클라이언트(app.js) - 메시지 출력 및 닉네임 입력 => type, payload를 키로 갖는  object타입 데이터 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const messageForm = document.querySelector("#message")
+const nickForm = document.querySelector("#nick")
 
 const socket = new WebSocket(`ws://${window.location.host}`)
 
@@ -11,21 +12,42 @@ socket.addEventListener("open", () => {
 
 // 메시지 수신
 socket.addEventListener("message", (message) => {
+  const li = document.createElement("li")
+  li.innerText = message.data;
+  messageList.append(li);
   console.log("Message from server ", message.data);
 });
 
 socket.addEventListener("close", () => {
-  socket.send("Connected from Server ❌"); // 메시지를 전송해도 받을 수 없다...?
+  socket.send("Disconnected from Server ❌"); // 메시지를 전송해도 받을 수 없다...?
 })
 
 /**
  * Enter혹은 send버튼클릭시 서버에 메시지 전송
  * @param {*} event 
  */
-function handleSubmit(event) {
+function handleMessageSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input")
-  socket.send(input.value);
+  socket.send(makeMessage('new_message', input.value));
+
   input.value = "";
 }
-messageForm.addEventListener("submit", handleSubmit)
+messageForm.addEventListener("submit", handleMessageSubmit)
+
+function makeMessage(type, payload) {
+  const msg = {type, payload}
+  return JSON.stringify(msg);
+}
+
+/**
+ * 
+ * @param {*} event 
+ */
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input")
+  socket.send(makeMessage('nickname', input.value));
+  input.value = "";
+}
+nickForm.addEventListener("submit", handleNickSubmit)
