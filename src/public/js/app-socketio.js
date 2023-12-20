c881a6f910afbbf592f8dba38d0aeb03082ce7d3
webdevyoo <yjk7454@naver.com>
2023-12-20 12:40:28 +0900

    클라이언트 script - emit 데이터,콜백 매개변수 수정 및 room영역 활성화 welcome영역 비활성화 콜백함수 정의

--- src/public/js/app-socketio.js
+++ src/public/js/app-socketio.js
@@ -8,8 +8,25 @@
 const socket = io();
 console.log(socket)
 
+/* View - Element */
 const welcome = document.getElementById("welcome")
-const form = document.querySelector("form")
+const form = welcome.querySelector("form")
+const room = document.getElementById("room")
+room.hidden = true // 룸 비활성화
+
+/**
+ * Room 영역을 활성화 시킨다
+ * 메시지를 입력하는 영역이 활성화된다.
+ * Welcom 영역을 비활성화 시킨다
+ * 룸정보를 입력하는 영역이 비활성화 된다.
+ */
+let roomname;
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false
+  const h3 = room.querySelector("h3")
+  h3.innerText = `Room ${roomname}`
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
@@ -22,12 +39,12 @@ function handleRoomSubmit(event) {
   /**
    * [emit] : socket.io에서 메시지 전송해주는 함수
    * 1번째 parameter: event(커스텀)로 사용
-   * 2번째 parameter: 데이터변환 없이 object자체로 전송가능
-   * 3번째 parameter: server에서 실행할 수 있는 callback함수
+   * 2번째 parameter~: 데이터변환 없이 object자체로 전송가능
+   * (3번째 4번째 등 데이터 arg 개수 제한없음)
+   * last parameter: server에서 실행할 수 있는 callback함수
    */
-  socket.emit("enter_room", {payload: input.value}, () => {
-    console.log("server is done")
-  });
+  roomname = input.value;
+  socket.emit("enter_room", roomname, showRoom);
   input.value = ""
 
 
