c056f2eeb3473162a640d9842214ab36bd257087
webdevyoo <yjk7454@naver.com>
2023-12-23 15:47:23 +0900

    카메라, 마이크로부터 출력되는 비디오,오디오를 video태그에 출력 및 음소거, 카메라전환 버튼 구현
    navigator의 mediaDevice로 부터 사용자의 비디오,오디오를 결합한 스트림을 얻은 후 video엘리먼트의 srcObject 속성을 통해 할당한다.
    이 작업을 통해 실시간으로 사용자의 카메라와 마이크로부터 음성과 비디오를 브라우저에 출력한다.

--- src/public/js/app-webRTC.js
+++ src/public/js/app-webRTC.js
@@ -1,2 +1,44 @@
 const socket = io();
 
+const myFace = document.getElementById("myFace")
+const muteBtn = document.getElementById("mute")
+const cameraBtn = document.getElementById("camera")
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getMedia() {
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({audio: false, video: true})
+    console.log(myStream)
+    myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error)
+  }
+}
+getMedia();
+
+function handleMuteClick(event) {
+  if(!muted) {
+    muteBtn.innerText = "UnMute"
+    muted = !muted
+    return
+  }
+  muteBtn.innerText = "Mute"
+  muted = !muted
+}
+
+function handleCameraClick(event) {
+  if(!cameraOff) {
+    cameraBtn.innerText = "Turn Camera On"
+    cameraOff = !cameraOff
+    return
+  }
+  cameraBtn.innerText = "Turn Camera Off"
+  cameraOff = !cameraOff
+
+}
+
+muteBtn.addEventListener("click", handleMuteClick)
+cameraBtn.addEventListener("click", handleCameraClick)
