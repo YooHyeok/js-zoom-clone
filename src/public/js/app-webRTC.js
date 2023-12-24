4f614102b325e1a993335d86e634281f7b881369
webdevyoo <yjk7454@naver.com>
2023-12-24 09:26:40 +0900

    음소거, 카메라전환 버튼 클릭시 enable반전

--- src/public/js/app-webRTC.js
+++ src/public/js/app-webRTC.js
@@ -3,15 +3,21 @@ const socket = io();
 const myFace = document.getElementById("myFace")
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
+const camerasSelect = document.getElementById("cameras")
 
 let myStream;
 let muted = false;
 let cameraOff = false;
 
+
+/**
+ * 미디어 스트림 비디오 할당
+ * getUserMedia() : 컴퓨터 혹은 모바일이 가지고 있는 미디어 스트림을 알려준다.
+ * 
+ */
 async function getMedia() {
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({audio: false, video: true})
-    console.log(myStream)
+    myStream = await navigator.mediaDevices.getUserMedia({audio: true, video: true})
     myFace.srcObject = myStream;
   } catch (error) {
     console.log(error)
@@ -20,6 +26,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick(event) {
+  myStream.getAudioTracks().forEach(track=>{
+    track.enabled = !track.enabled
+  })
   if(!muted) {
     muteBtn.innerText = "UnMute"
     muted = !muted
@@ -30,6 +39,10 @@ function handleMuteClick(event) {
 }
 
 function handleCameraClick(event) {
+  myStream.getVideoTracks().forEach(track=>{
+    console.log(track)
+    track.enabled = !track.enabled
+  })
   if(!cameraOff) {
     cameraBtn.innerText = "Turn Camera On"
     cameraOff = !cameraOff
